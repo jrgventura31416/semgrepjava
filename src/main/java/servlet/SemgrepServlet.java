0fa37b915a1252494f4efcd20d53a9341ec5ec42
Jorge Ventura <jorge.venturag@outlook.com>
2021-11-23 15:10:55 -0500

    print systema

---
+++ src/main/java/servlet/SemgrepServlet.java
@@ -0,0 +1,51 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SemgrepServlet
+ */
+@WebServlet("/SemgrepServlet")
+public class SemgrepServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SemgrepServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    	
+    	String email = request.getParameter("txtEmail");
+    	String password = request.getParameter("txtPassword");
+    	
+    	System.out.print(email);
+    	System.out.println(email);
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
